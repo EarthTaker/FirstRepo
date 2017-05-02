ef5e5781f85f0e7821ed2bcfdbe9eb95c7bfeefa
Garrison Geho <Talkater2010@hotmail.com>
2017-05-01 22:49:11 -0500

    My secomd commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by EarthTaker on 5/1/2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Finally got this stupid commit stuff to work!!!!");
+    }
+}
